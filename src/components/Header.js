6d54b396c1559da600ba164c262728c690a2499e
shaunvan119 <shaunvan@live.com.au>
2021-05-15 16:53:31 +1000

    added Header and components folder

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+
+const Header = (props) => {
+    return (
+        <header>
+          <h1>{props.title}</h1>  
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+export default Header
