9dcba24eb3737ccbe179e70ee035faed6686454b
shaunvan119 <shaunvan@live.com.au>
2021-05-18 07:21:18 +1000

    Updated Button file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 
 import PropTypes from 'prop-types'
+import Button from './Button'
  
 
 const Header = ({title})=> {
+    const onClick = () => {
+        console.log('Click')
+    }
     return (
-        <header>
-          <h1>{title}</h1>  
+        <header className='header'>
+          <h1>{title}</h1> 
+          <Button color='green' text='Add' onClick={onClick}/>
+          
+
         </header>
+        
     )
 }
 
